2615abf186729a90f14dab8e65c39f30317cf8b1
bswebdvlpr <tfl0324@gachon.ac.kr>
2023-03-25 14:32:54 +0900

    First Commit

---
+++ express_test/app.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ msg: "express test" });
+});
+
+app.use("/uploads", (req, res) => {
+  fs.readFile(`./uploads${req.path}`, (err, data) => {
+    if (err) {
+      res.send({
+        readFileUrl: `./uploads${req.path}`,
+        path: req.path,
+        url: req.url,
+        originalUrl: req.originalUrl,
+        headersHost: req.headers.host,
+        err,
+      });
+    }
+    res.send(data);
+  });
+});
+
+app.listen(3000, () => {
+  console.log(`Server Listening on 3000`);
+});
+
+module.exports = app;
