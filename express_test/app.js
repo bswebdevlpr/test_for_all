974f4f14b27e688add7f4c5c7d70b5373d3d6ccb
bswebdvlpr <tfl0324@gachon.ac.kr>
2023-04-04 18:42:00 +0900

    Init: graphql-test

--- express_test/app.js
+++ express_test/app.js
@@ -2,28 +2,32 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 
+app.use("/uploads", express.static("./uploads"));
+
 app.get("/", (req, res) => {
   res.json({ msg: "express test" });
 });
 
-app.use("/uploads", (req, res) => {
-  fs.readFile(`./uploads${req.path}`, (err, data) => {
-    if (err) {
-      res.send({
-        readFileUrl: `./uploads${req.path}`,
-        path: req.path,
-        url: req.url,
-        originalUrl: req.originalUrl,
-        headersHost: req.headers.host,
-        err,
-      });
-    }
-    res.send(data);
-  });
+app.get("/test", (req, res) => {
+  res.json({ msg: "TEST URL" });
 });
+
+// app.use("/uploads", (req, res) => {
+//   fs.readFile(`./uploads${req.path}`, (err, data) => {
+//     if (err) {
+//       res.send({
+//         readFileUrl: `./uploads${req.path}`,
+//         path: req.path,
+//         url: req.url,
+//         originalUrl: req.originalUrl,
+//         headersHost: req.headers.host,
+//         err,
+//       });
+//     }
+//     res.send(data);
+//   });
+// });
 
 app.listen(3000, () => {
   console.log(`Server Listening on 3000`);
 });
-
-module.exports = app;
