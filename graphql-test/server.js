6100ef48120f45f3b5a1dd737bb57b0930923714
bswebdvlpr <tfl0324@gachon.ac.kr>
2023-04-08 15:49:57 +0900

    Uplaod: graphql-test

--- graphql-test/server.js
+++ graphql-test/server.js
@@ -1,14 +1,31 @@
 import { ApolloServer, gql } from "apollo-server";
 
-const server = new ApolloServer({});
+const tweets = [
+  {
+    id: 1,
+    text: "first one",
+  },
+  {
+    id: 2,
+    text: "second one",
+  },
+];
 
-// Query는 REST API에서 GET request를 만드는 것과 같다. /text, GET /hello와 같다.
+const users = [
+  {
+    id: 1,
+    firstName: "k",
+    lastName: "bs",
+  },
+];
+
+// Query는 REST API의 GET과 비슷하다. Query 내에 정의되면 REST API에서 링크로 접근하는 것처럼 적용된다.
 const typeDefs = gql`
   type User {
     id: ID!
-    username: String!
     firstName: String!
     lastName: String!
+    fullName: String!
   }
   type Tweet {
     id: ID!
@@ -16,6 +33,7 @@ const typeDefs = gql`
     author: User!
   }
   type Query {
+    allUsers: [User!]!
     allTweets: [Tweet!]!
     tweet(id: ID!): Tweet
   }
@@ -27,11 +45,36 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    tweet() {
+    allUsers() {
+      return users;
+    },
+    allTweets() {
+      return tweets;
+    },
+    tweet(root, args) {
+      console.log(args);
       return null;
     },
   },
+  Mutation: {
+    postTweet(root, { text, userId }) {
+      const newTweet = {
+        id: tweets.length + 1,
+        text: text,
+      };
+
+      tweets.push(newTweet);
+      return newTweet;
+    },
+  },
+  User: {
+    fullName({ firstName, lastName }) {
+      return `${firstName} ${lastName}`;
+    },
+  },
 };
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
